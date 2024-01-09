f05bc4598427255adb0d4721c0ab7941c5e8fe5d
Henrique Albuquerque <henrique.albuquerque.248@gmail.com>
2024-01-09 14:25:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
-const http = require('http');
-const PORT = 3000;
+const express = require('express')
+const http = require('http')
+const app = express()
+const server = http.createServer(app)
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
-});
+import { Server } from 'socket.io'
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
-});
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+    },
+})
+
+type Point = { x: number, y: number}
+
+type DrawLine = {
+    prevPoint: Point | null
+    currentPoint: Point
+    color: string
+}
+
+io.on('connection', (socket) => {
+    console.log("connection")
+
+    socket.on('client-ready', () => {
+        socket.broadcast.emit('get-canvas-state')
+    })
+
+    socket.on('canvas-state', (state) => {
+        socket.broadcast.emit('canvas-state-from-server', state)
+    })
+
+    socket.on('draw-line', ({prevPoint, currentPoint, color}: DrawLine) => {
+        socket.broadcast.emit('draw-line', { prevPoint, currentPoint, color })
+    })
+
+
+    socket.on('clear', () => io.emit('clear'))
+})
+
+server.listen(3001, () => {
+    console.log('Server listening on port 3001')
+})
